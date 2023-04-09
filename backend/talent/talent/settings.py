dfbefa8b5e756c32a2f61fbf2ff012d74dd7d1a9
Kevin Munene <60537956+muneneee@users.noreply.github.com>
2023-04-09 14:24:46 +0300

    Update settings.py

--- backend/talent/talent/settings.py
+++ backend/talent/talent/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l8@0=wt^ttl6nnj!q(wk(l^ug^hme*63*8vs#)o*)cc671d7jy'
+SECRET_KEY = '[SECRET_KEY]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,17 +90,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'talanta',
-        'USER': 'postgres',
-        'PASSWORD': '12330122015',
+        'USER': '[dbUser]',
+        'PASSWORD': ['Password]',
         'HOST': 'localhost'
     }
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'kevinmunene543@gmail.com'
-EMAIL_HOST_PASSWORD = 'elvhvvdwqacxoxhx'
+EMAIL_HOST = '[host]'
+EMAIL_PORT = '[port]'
+EMAIL_HOST_USER = '[email]'
+EMAIL_HOST_PASSWORD = '[password]'
 EMAIL_USE_TLS = True
 
 # Password validation
